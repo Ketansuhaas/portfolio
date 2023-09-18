0d5bb143b4b5c01e8508fdc3c6426b13eb14891c
Ketansuhaas <ketansuhaas@gmail.com>
2023-09-17 20:06:09 -0400

    again and again

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -3,33 +3,37 @@ st.set_page_config(
     layout="wide",
     page_title="Projects"
 )
+st.markdown("""
+<style>
+.big-font {
+    font-size:20px !important;
+}
+.head-font {
+    font-size:25px !important;
+}
+</style>
+""", unsafe_allow_html=True)
 st.title("Projects")
 c1, c2 ,c3 ,c4, c5 = st.container(), st.container() , st.container(), st.container(), st.container()
 #c1c2.image("Slice.png",width=400)
 c1.header("Cardiac MRI Image Segmentation using Deep Learning")
 c1.image("cis.jpg")
-st.markdown("""
-<style>
-.big-font {
-    font-size:25px !important;
-}
-</style>
-""", unsafe_allow_html=True)
-c1.markdown('<p class="big-font"><i>(August 2022 - May 2023)</i> <ul>\
+
+c1.markdown('<p class="big-font">(August 2022 - May 2023) <ul>\
             <li>Implemented advanced U-Net-based attention models in TensorFlow for the semantic segmentation of 3D Cardiac short-axis MRI Images.</li>\
             <li>Conducted a thorough comparison of model results and recorded performance benchmarks.</li>\
             <li>Achieved a significantly high dice coefficient on the M&Ms Dataset.</li>\
-            <li>Designed an interactive web application using Streamlit framework in Python.</li></p>', unsafe_allow_html=True)
+            <li>Designed an interactive web application using Streamlit framework in Python.</li></ul></p>', unsafe_allow_html=True)
 c1.write("GitHub repository is [here.](https://github.com/Ketansuhaas/cardiac-image-segmentation)")
 c1.write("Pre-print is [here.](https://github.com/Ketansuhaas/cardiac-image-segmentation/blob/main/Final%20BTP%20report%20for%20masters.pdf)")
 
 
 c2.header("arXiv Research Article Recommender")
 c2.image("ar.jpg")
-c2.markdown('<p class="big-font"><i>(August 2023 - Present)</i> <ul>\
+c2.markdown('<p class="big-font">(August 2023 - Present) <ul>\
             <li>Leveraged BERT embeddings to encode titles from a subset of the arXiv dataset.</li>\
             <li>Currently working on utilizing abstracts for improved results, albeit with higher computational demands.</li>\
-            <li>Developed a K-Means model for recommending related research articles.</li><li>Designed and deployed an elegant web application using Streamlit.</li></p>', unsafe_allow_html=True)
+            <li>Developed a K-Means model for recommending related research articles.</li><li>Designed and deployed an elegant web application using Streamlit.</li></ul></p>', unsafe_allow_html=True)
 c2.write("GitHub repository is [here.](https://github.com/Ketansuhaas/arXiv-article-recommender)")
 c2.write("Access the web-app [here.](https://arxiv-article-recommender-8uq7kkdrefhdsf8m5xkge7.streamlit.app/)")
 
@@ -38,24 +42,25 @@ c3.header("Next Word Predictor")
 c31,c32 = c3.columns(2)
 c31.image("nw1.jpg")
 c32.image("nw2.jpg")
-c3.markdown('<p class="big-font"><i>(May 2023 - August 2023)</i> <ul>\
+c3.markdown('<p class="big-font">(May 2023 - August 2023)<ul>\
             <li>Implemented a stacked LSTM model in Keras for predicting the next possible words of a sentence.</li>\
-            <li>Designed an elegant multipage web application using Streamlit that also allows training the model on a context text before prediction.</li>\
-            <li>Displays the probability of all the words that could appear next.</li></p>', unsafe_allow_html=True)
+            <li>Designed an elegant multipage web application using Streamlit.</li>\
+            <li>Allows training the model on a context text before prediction.</li>\
+            <li>Displays the probability of all the words that could appear next.</li></ul></p>', unsafe_allow_html=True)
 c3.write("GitHub repository is [here.](https://github.com/Ketansuhaas/next-word-predictor)")
 
 
 c4.header("Hand-gesture-controlled mouse using Computer Vision")
 c4.image("Aimouse.jpg")
-c4.markdown('<p class="big-font"><i>(October 2022 - November 2022)</i> <ul>\
+c4.markdown('<p class="big-font">(October 2022 - November 2022) <ul>\
             <li>Developed an end-to-end desktop AI mouse application using the Pyinstaller library.</li>\
             <li>Leveraged the MediaPipe library for precise finger coordinate detection and devised algorithms for recognizing physical hand gestures.</li>\
-            <li>Incorporated the AutoPy library for intuitive mouse pointer control.</li></p>', unsafe_allow_html=True)
+            <li>Incorporated the AutoPy library for intuitive mouse pointer control.</li></ul></p>', unsafe_allow_html=True)
 c4.write("GitHub repository is [here.](https://github.com/Ketansuhaas/AIMouse)")
 
 
 c5.header("Solving Economic Load Dispatch Problem On Quantum Computers   ")
-c5.markdown('<p class="big-font"><i>(January 2022 - March 2022)</i> <ul>\
+c5.markdown('<p class="big-font">(January 2022 - March 2022) <ul>\
             <li>Economic Load Dispatch involves the optimal allocation of generation \
             units across all generators, satisfying the power, voltage, transmission line,\
              and network security constraints.</li>\
@@ -63,5 +68,5 @@ c5.markdown('<p class="big-font"><i>(January 2022 - March 2022)</i> <ul>\
             solver on the D-Wave Leap IDE in Python for computation.</li>\
             <li>Successfully reduced the time of computation significantly \
             (in the range of 10^3 sec) for solving the economic load dispatch problem, \
-            proving the efficacy of quantum computers over classical ones.</li></p>', unsafe_allow_html=True)
+            proving the efficacy of quantum computers over classical ones.</li></ul></p>', unsafe_allow_html=True)
 c5.write("Project report is [here.](https://drive.google.com/file/d/1d3f8KkhOTx2XdEwzirhHxv2sQE_z9cuh/view?usp=sharing)")
