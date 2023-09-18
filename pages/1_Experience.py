82b3e226e21194dca4f77f7cb88e26cbae501e64
Ketansuhaas <ketansuhaas@gmail.com>
2023-09-17 20:01:24 -0400

    again and again

--- pages/1_Experience.py
+++ pages/1_Experience.py
@@ -18,13 +18,13 @@ st.markdown("""
 }
 </style>
 """, unsafe_allow_html=True)
-c1c1.markdown('<p class="big-font"><i>Software Development Intern (May 2022 - July 2022)</i> <ul><li>Contributed as a backend engineer within the payment gateway service team.</li><li>Designed and implemented APIs, seamlessly integrating Juspay endpoints for customer order creation and order status retrieval using Springboot in Java.</li><li>Played a pivotal role in optimizing the backend system and enhancing the fraud detection system through the application of machine learning techniques.</li></p>', unsafe_allow_html=True)
+c1c1.markdown('<p class="big-font">Software Development Intern (May 2022 - July 2022)<ul><li>Contributed as a backend engineer within the payment gateway service team.</li><li>Designed and implemented APIs, seamlessly integrating Juspay endpoints for customer order creation and order status retrieval using Springboot in Java.</li><li>Played a pivotal role in optimizing the backend system and enhancing the fraud detection system through the application of machine learning techniques.</li></p>', unsafe_allow_html=True)
 
 
 
 c2c2.image("IITR.png", width=180)
 c2c1.header("Machine Learning Lab, EE Dept, IIT Roorkee")
-c2c1.markdown('<p class="big-font"><i>Research Apprentice (August 2022 - May 2023)</i> <ul><li>Collaborated closely with a Ph.D. student to delve into semantic segmentation for biomedical images.\
+c2c1.markdown('<p class="big-font">Research Apprentice (August 2022 - May 2023)<ul><li>Collaborated closely with a Ph.D. student to delve into semantic segmentation for biomedical images.\
               <li>Extensively analyzed and implemented the nn-U-Net architecture, meticulously validating its performance benchmarks through \
               rigorous training.</li>\
               <li>Assisted in designing and testing a new \
--- pages/2_Education.py
+++ pages/2_Education.py
@@ -20,7 +20,7 @@ c1c1,c1c2 = c1.columns(2)
 c2c1,c2c2 = c2.columns(2)
 
 c1c2.image("BU.png",width=180)
-c1c1.markdown('<p class="head-font">Boston University</p>', unsafe_allow_html=True)
+c1c1.markdown('<p class="head-font"><b>Boston University</b></p>', unsafe_allow_html=True)
 
 c1c1.markdown('<p class="big-font">Masters in Artificial Intelligence (2023 - Present)</p>', unsafe_allow_html=True)
 
@@ -28,5 +28,5 @@ c1c1.markdown('<p class="big-font">Masters in Artificial Intelligence (2023 - Pr
 
 
 c2c2.image("IITR.png", width=180)
-c2c1.markdown('<p class="head-font">Indian Institute of Technology (IIT) Roorkee</p>', unsafe_allow_html=True)
+c2c1.markdown('<p class="head-font"><b>Indian Institute of Technology (IIT) Roorkee</b></p>', unsafe_allow_html=True)
 c2c1.markdown('<p class="big-font">Bachelor of Technology in Electrical Engineering (2019 - 2023)<br>CGPA : 8.645/10</p>', unsafe_allow_html=True)
